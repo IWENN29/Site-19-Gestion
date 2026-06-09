d78acab7232fef54a91b879627410f7385e5f747
IWENN29 <150153911+IWENN29@users.noreply.github.com>
2026-06-10 01:59:51 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -12,7 +12,7 @@ export const botConfig = {
   // - "invisible" = appears offline
   presence: {
     // Current online state shown on Discord.
-    status: "idle",
+    status: "online",
 
     // Activity lines shown under the bot name.
     // `type` number mapping from Discord:
@@ -24,10 +24,10 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        // Text users will see (example: "Playing /help | Site-19 - Gestion Bot").
+        name: "Protege les Serveurs",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
